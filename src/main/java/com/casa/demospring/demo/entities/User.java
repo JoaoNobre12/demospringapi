77d79e85a08407d59c02a27758089203880bce85
joao nobre <joaovitornobre12@hotmail.com>
2024-08-08 02:38:06 -0300

    configures h2

--- src/main/java/com/casa/demospring/demo/entities/User.java
+++ src/main/java/com/casa/demospring/demo/entities/User.java
@@ -1,5 +1,6 @@
 package com.casa.demospring.demo.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
@@ -10,10 +11,15 @@ import java.io.Serializable;
 @NoArgsConstructor
 @ToString
 @EqualsAndHashCode
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
+
 }
