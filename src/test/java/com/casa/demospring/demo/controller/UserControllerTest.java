fadd248214ee1470bc3cc29e7ab9407d77c087d1
joao nobre <joaovitornobre12@hotmail.com>
2024-09-13 23:49:36 -0300

    removes unutilized code

--- src/test/java/com/casa/demospring/demo/controller/UserControllerTest.java
+++ src/test/java/com/casa/demospring/demo/controller/UserControllerTest.java
@@ -23,9 +23,6 @@ class UserControllerTest {
     private MockMvc mockMvc;
 
     @Mock
-    private UserRepository userRepository;
-
-    @Mock
     private UserService userService;
 
     @InjectMocks
@@ -36,18 +33,15 @@ class UserControllerTest {
 
     @Test
     void testFindAll() throws Exception {
-        // Configura o mock para retornar uma lista de usuários
         List<User> users = List.of(
                 new User(1L, "John Doe", "john@example.com","123456","123456"));
         when(userService.findAll()).thenReturn(users);
 
-        // Configura o MockMvc com o Controller e o ControllerAdvice
         mockMvc = MockMvcBuilders
                 .standaloneSetup(userController)
                 .setControllerAdvice(restCustomException)
                 .build();
 
-        // Faz a requisição e verifica se o status code é 200 (OK)
         mockMvc.perform(get("/users"))
                 .andExpect(status().isOk());
     }
