cf616af649df24cc482ac6bb50d8ebbea4bc5589
joao nobre <joaovitornobre12@hotmail.com>
2024-09-14 00:09:00 -0300

    finishes test

--- src/test/java/com/casa/demospring/demo/controller/UserControllerTest.java
+++ src/test/java/com/casa/demospring/demo/controller/UserControllerTest.java
@@ -1,21 +1,27 @@
 package com.casa.demospring.demo.controller;
 
+import com.casa.demospring.demo.controller.exceptions.ControllerExceptionHandler;
 import com.casa.demospring.demo.entities.User;
 import com.casa.demospring.demo.exceptions.RestCustomException;
 import com.casa.demospring.demo.repositories.UserRepository;
 import com.casa.demospring.demo.services.UserService;
+import com.casa.demospring.demo.services.exceptions.ConflictException;
+import com.casa.demospring.demo.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,7 +35,15 @@ class UserControllerTest {
     private UserController userController;
 
     @InjectMocks
-    private RestCustomException restCustomException;
+    private ControllerExceptionHandler controllerExceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(userController)
+                .setControllerAdvice(controllerExceptionHandler)
+                .build();
+    }
 
     @Test
     void testFindAll() throws Exception {
@@ -37,12 +51,50 @@ class UserControllerTest {
                 new User(1L, "John Doe", "john@example.com","123456","123456"));
         when(userService.findAll()).thenReturn(users);
 
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(userController)
-                .setControllerAdvice(restCustomException)
-                .build();
-
         mockMvc.perform(get("/users"))
                 .andExpect(status().isOk());
     }
-}
+
+    @Test
+    void testFindById() throws Exception {
+        User user = new User(1L, "John Doe", "john@example.com","123456","123456");
+        when(userService.findById(1L)).thenReturn(user);
+
+        mockMvc.perform(get("/users/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testFindByIdNotFound() throws Exception {
+        when(userService.findById(1L)).thenThrow(new ResourceNotFoundException(1L));
+
+        mockMvc.perform(get("/users/1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testDelete() throws Exception {
+        mockMvc.perform(delete("/users/1"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void testDeleteConflict() throws Exception {
+
+        doThrow(new ConflictException(1L)).when(userService).delete(1L);
+
+        mockMvc.perform(delete("/users/1"))
+                .andExpect(status().isConflict());
+    }
+
+@Test
+void testUpdate() throws Exception {
+    User updatedUser = new User(1L, "John Doe Updated", "john.updated@example.com", "654321", "654321");
+
+    when(userService.update(1L, updatedUser)).thenReturn(updatedUser);
+
+    mockMvc.perform(put("/users/1")
+            .contentType("application/json")
+            .content("{\"id\":1,\"name\":\"John Doe Updated\",\"email\":\"john.updated@example.com\",\"phone\":\"654321\",\"password\":\"654321\"}"))
+            .andExpect(status().isOk());
+}}
